b5fcb3f840d98d3b87b1845866ab6f1b499bc2e5
Csengeee <szekely.csenge2001@gmail.com>
2026-02-26 15:06:18 +0100

    Update NotesController.js

--- controllers/NotesController.js
+++ controllers/NotesController.js
@@ -1,4 +1,4 @@
-const { connectDB } = require("../config/db");
+const { connectDB, getConnection } = require("../config/db");
 const sanitizer = require('../modules/inputSanitizer');
 
 // Segédfüggvény a jegyzetek egységes lekéréséhez
